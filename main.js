9f16443a10b708aa85b6f570fcd17a09d40f79bf
HeekyungKim6424 <human09129@sookmyung.ac.kr>
2021-06-06 19:15:29 +0900

    css제작 및 js 개요 작성

---
+++ main.js
@@ -0,0 +1,9 @@
+function freshman(){
+    alert("check your domitory clicking site blow the button 'ok'");
+}
+
+function ok(){
+    var name=document.getElementById("name").value;
+    var domi=document.getElementById("domi").value;
+    alert("Congratulations on admission!");
+}
